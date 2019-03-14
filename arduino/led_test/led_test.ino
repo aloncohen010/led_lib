2ea0a2b2455cd234cbd34a781c38177074ba117d
aloncohen010 <43686116+aloncohen010@users.noreply.github.com>
2019-03-14 16:26:41 +0200

    more bug fixes

--- arduino/led_test/led_test.ino
+++ arduino/led_test/led_test.ino
@@ -2,26 +2,29 @@
 LED led(11, 0);
 int func = 0;
 
-void setup() {
-}
+void setup() {}
 
 void loop() {
-  if ((millis() / 1000) % 10 == 0) {
-    switch (func) {
-      case 0:
-        led.setBlink(1000);
-        break;
-      case 1:
-        led.setFlicker(20);
-        break;
-      case 2:
-        led.setTransition(255, 20);
-        break;
-      case 3:
-        led.setPulse(0.0008);
-        break;
-    }
-    func = func > 3 ? 0 : func + 1;
+  delay(1000 * 10);
+  switch (func) {
+  case 0:
+    led.setBlink(1000);
+    func++;
+    break;
+  case 1:
+    led.setFlicker(20);
+    func++;
+    break;
+  case 2:
+    led.setTransition(255, 20);
+    func++;
+    break;
+  case 3:
+    led.setPulse(0.0008);
+    func++;
+    break;
   }
-  led.update();
+  func = func > 3 ? 0 : func + 1;
+}
+led.update();
 }
--- arduino/rgb_led_test/rgb_led_test.ino
+++ arduino/rgb_led_test/rgb_led_test.ino
@@ -1,27 +1,31 @@
 #include "RGB_LED.h"
 
-RGB_LED rgb_led(9, 10, 11, COLOR(0, 0, 0));
+RGB_LED rgb_led(11, 10, 9, COLOR(0, 0, 0));
 int func = 0;
 
 void setup() { pinMode(8, OUTPUT); }
 
 void loop() {
-  if ((millis() / 1000) % 60 == 0) {
-    switch (func) {
-    case 0:
-      rgb_led.setColorTransition(COLOR(255, 255, 0), 0.0025, 0.0025, 0.0025);
-      break;
-    case 1:
-      rgb_led.setColorTransition(COLOR(255, 0, 255), 0.0025, 0.0025, 0.0025);
-      break;
-    case 2:
-      rgb_led.setColorTransition(COLOR(0, 255, 255), 0.0025, 0.0025, 0.0025);
-      break;
-    case 3:
-      rgb_led.setColorTransition(COLOR(128, 0, 255), 0.0025, 0.0025, 0.0025);
-      break;
-    }
-    func = func > 3 ? 0 : func + 1;
+  delay(1000 * 10);
+  switch (func) {
+  case 0:
+    rgb_led.setColorTransition(COLOR(255, 255, 0), 20, 20, 20);
+    func++;
+    break;
+  case 1:
+    rgb_led.setColorTransition(COLOR(255, 0, 255), 20, 20, 20);
+    func++;
+    break;
+  case 2:
+    rgb_led.setColorTransition(COLOR(0, 255, 255), 20, 20, 20);
+    func++;
+    break;
+  case 3:
+    rgb_led.setColorTransition(COLOR(128, 0, 255), 20, 20, 20);
+    func++;
+    break;
   }
-  rgb_led.update();
+  func = func > 3 ? 0 : func + 1;
+}
+rgb_led.update();
 }
